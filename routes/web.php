bc96fc218014b2d98c214ecac2389909b7765f73
David Peach <mail@davidpeach.co.uk>
2024-04-16 21:57:30 +0100

    Add a scrobble count endpoint.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\Listen;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('scrobble-count', function () {
+    return Listen::count();
 });
