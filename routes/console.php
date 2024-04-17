658e807c45ff9c4d88ef0672daaeb1d4296f13bd
David Peach <mail@davidpeach.co.uk>
2024-04-17 14:13:12 +0100

    Remove past scrobble importer. All old ones now imported.

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Console\Commands\ImportNewScrobblesCommand;
-use App\Console\Commands\ImportPastScrobblesCommand;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(ImportPastScrobblesCommand::class)->everyMinute();
 Schedule::command(ImportNewScrobblesCommand::class)->everyMinute();
