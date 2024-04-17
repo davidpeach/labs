a0558b101832f08b439df6c371b9e9bbae35868c
David Peach <mail@davidpeach.co.uk>
2024-04-17 14:11:45 +0100

    Add new command to schedule.

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Console\Commands\ImportNewScrobblesCommand;
 use App\Console\Commands\ImportPastScrobblesCommand;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(ImportPastScrobblesCommand::class)->everyMinute();
+Schedule::command(ImportNewScrobblesCommand::class)->everyMinute();
