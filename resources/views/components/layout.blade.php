2b551a06e5528fa6a8017078104fa3849ab08181
David Peach <mail@davidpeach.co.uk>
2024-04-22 12:57:41 +0100

    Add mobile nav open button

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,6 +13,15 @@
 
     <div x-data="{showMobileMenu: false}">
 
+        <span class="bg-green-50 absolute lg:hidden right-0 p-4 cursor-pointer"
+            @click="showMobileMenu = !showMobileMenu">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-10 h-10">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+
+        </span>
+
         <div x-cloak :class="
                 showMobileMenu ?
                 'relative z-50 lg:hidden transition-opacity ease-linear duration-300 opacity-100':
