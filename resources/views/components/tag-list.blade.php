05794c23138a00eaafd5a5d74b88cd1f1356235a
David Peach <mail@davidpeach.co.uk>
2024-05-02 15:12:37 +0100

    Style tweaks for posts pages and tag list

--- resources/views/components/tag-list.blade.php
+++ resources/views/components/tag-list.blade.php
@@ -1,7 +1,7 @@
 @unless($tags->isEmpty())
-    <p class="bg-blue-500 text-white p-1">
+    <p class=""><span class="sr-only">Tags</span>
     @foreach($tags as $tag)
-        <span>{{ $tag->name }}</span> |
+        <span class=" bg-purple-700 rounded-lg text-white inline-block text-lg px-4 py-2 mr-2">{{ $tag->name }}</span>
     @endforeach
     </p>
 @endunless
