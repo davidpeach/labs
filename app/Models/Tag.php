f6877b7dbf956230c5721adf96f634a12b268222
David Peach <mail@davidpeach.co.uk>
2024-05-02 15:20:48 +0100

    Add tag permalink and link the post tags.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Tags\Tag as BaseTag;
 
 class Tag extends BaseTag
 {
     use HasFactory;
+
+    public function permalink(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => config('app.url').'/tags/'.$this->slug,
+        );
+    }
 }
--- resources/views/components/tag-list.blade.php
+++ resources/views/components/tag-list.blade.php
@@ -1,7 +1,7 @@
 @unless($tags->isEmpty())
     <p class=""><span class="sr-only">Tags</span>
     @foreach($tags as $tag)
-        <span class=" bg-purple-700 rounded-lg text-white inline-block text-lg px-4 py-2 mr-2">{{ $tag->name }}</span>
+        <a href="{{ $tag->permalink }}" class=" bg-purple-700 rounded-lg text-white inline-block text-lg px-4 py-2 mr-2">{{ $tag->name }}</a>
     @endforeach
     </p>
 @endunless
