4f32c79375fbb266e070eea4efb214e45f83b5dd
David Peach <mail@davidpeach.co.uk>
2024-05-16 22:00:40 +0100

    Fix tag permalink.

--- resources/views/components/tag-list.blade.php
+++ resources/views/components/tag-list.blade.php
@@ -1,7 +1,7 @@
 @unless($tags->isEmpty())
     <p class="py-8"><span class="sr-only">Tags</span>
     @foreach($tags as $tag)
-        <a href="{{ $tag->pemalink }}" class="bg-yellow-100 py-1 px-2 lg:py-2 lg:px-4 inline-block hover:bg-yellow-400 uppercase">{{ $tag->name }}</a>
+        <a href="{{ $tag->permalink }}" class="bg-yellow-100 py-1 px-2 lg:py-2 lg:px-4 inline-block hover:bg-yellow-400 uppercase">{{ $tag->name }}</a>
     @endforeach
     </p>
 @endunless
