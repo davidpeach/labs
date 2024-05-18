ed033e8e2370443916ae4692702c82b9f991f25b
David Peach <mail@davidpeach.co.uk>
2024-05-18 21:45:31 +0100

    Add now page back in.

---
+++ resources/views/v1/now.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <div class="@container">
+        <x-paragraph>
+            This is my NOW page
+        </x-paragraph>
+
+        <x-paragraph>
+            <ul>
+                <li>Building: this website</li>
+                <li>Working: Get Laid Beds</li>
+                <li>Watching: Final series of Inside Number 9 😢</li>
+            </ul>
+        </x-paragraph>
+
+
+    </div>
+</x-layout>
